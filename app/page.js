9651c31a8cad3b5652c75e63bf9db7044eea0af0
AlexAlstr <jampingkuda2@gmail.com>
2026-09-21 15:03:52 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ import StyleTab from './components/StyleTab';
 import PageTab from './components/PageTab';
 import DataTab from './components/DataTab';
 import PreviewPanel from './components/PreviewPanel';
-import ImportModal from './components/ImportModal';
+import ImportpdfModal from './components/ImportModal';
 
 export default function Home() {
   const [doc, setDoc] = useState(buildInitialDoc);
